0661beb687d67b24c6fb540529a219f8b91f206d
Christina <mail@christinaandersen.ch>
2023-04-12 10:46:03 +0200

    Event handler for cashing added

---
+++ sw.js
@@ -0,0 +1,19 @@
+const cashName = 'cash-v1'
+const resourcesToPrecash = [
+	'/',
+	'index.html',
+	'images/wiederstands_blank.gif',
+	'js/script.js'
+	'js/functions.js'
+];
+
+
+self.addEventListener ('install'. event => {
+	console. log('Service worker install event!');
+		event.waitUntil(
+			caches.open (cacheName)
+			.then (cache => {
+				return cache.addAll (resourcesToPrecache);
+		})
+	);
+});
