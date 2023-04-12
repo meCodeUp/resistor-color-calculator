e42cac49b6b37c51340c17fa82e776168e22a015
Christina <mail@christinaandersen.ch>
2023-04-12 11:39:01 +0200

    new chach worker

--- sw.js
+++ sw.js
@@ -1,16 +1,10 @@
-this.addEventListener("install", (event) => {
+this.addEventListener('install', function(event) {
   event.waitUntil(
-    caches
-      .open("caches-v1")
-      .then((cache) =>
-        cache.addAll([
-		'/resistor-color-calculator/',
-		'/resistor-color-calculator/index.html',
-		'/resistor-color-calculator/images/wiederstands_blank.gif',
-		'/resistor-color-calculator/css/style.css',
-		'/resistor-color-calculator/js/script.js',
-		'/resistor-color-calculator/js/functions.js',
-        ])
-      )
+    caches.open('cashe-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'index.html'
+      ]);
+    })
   );
 });
